def710876d525b6acdab60a0ff0e43dc1cabb758
Ilija Krsteski <krsteski@Ilijas-MBP.fritz.box>
2020-09-25 14:49:53 +0200

    finished signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { FooterContainer } from "../containers/footer.container";
+import { HeaderContainer } from "../containers/header.container";
+import { Form } from "../components";
 
 export default function Signin() {
-  return <p>Hi from the sign in</p>;
+  const [emailAdress, setEmailAdress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // check form input elemtns are valid
+  // email & password
+
+  const isInvalid = password === "" || emailAdress === "";
+  const handleSignIn = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              type="email"
+              value={emailAdress}
+              onChange={({ target }) => setEmailAdress(target.value)} //
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
